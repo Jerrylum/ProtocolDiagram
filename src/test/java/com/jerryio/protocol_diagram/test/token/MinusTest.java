87e9e2760ba350f260bfcb475c437d3045797024
crapper <crapper.612@gmail.com>
2022-09-26 13:02:55 +0800

    test: Add MinusTest

--- src/test/java/com/jerryio/protocol_diagram/test/token/MinusTest.java
+++ src/test/java/com/jerryio/protocol_diagram/test/token/MinusTest.java
@@ -9,11 +9,19 @@ import com.jerryio.protocol_diagram.token.Minus;
 
 public class MinusTest {
     @Test
-    public void testMinusParsing() {
-        assertEquals(Minus.parse(new CodePointBuffer("")), null);
-        assertEquals(Minus.parse(new CodePointBuffer("0")), null);
-        assertEquals(Minus.parse(new CodePointBuffer(" -")), null);
+    public void testMinusVaild() {
+        
         assertEquals(Minus.parse(new CodePointBuffer("-")).getClass(), Minus.class);
         assertEquals(Minus.parse(new CodePointBuffer("- ")).getClass(), Minus.class);
+    }
+    @Test
+    public void testMinusNull() {
+        assertNull(Minus.parse(new CodePointBuffer("")));
+        assertNull(Minus.parse(new CodePointBuffer("0")));
+        assertNull(Minus.parse(new CodePointBuffer(" -")));
+        assertNull(Minus.parse(new CodePointBuffer("a")));
+        assertNull(Minus.parse(new CodePointBuffer("1")));
+        assertNull(Minus.parse(new CodePointBuffer("a-")));
+        assertNull(Minus.parse(new CodePointBuffer("1-")));
     }
 }
