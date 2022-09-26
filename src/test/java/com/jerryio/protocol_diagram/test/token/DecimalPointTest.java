96c37dbf7eb854dd581d68636b6122d2c90e4936
crapper <crapper.612@gmail.com>
2022-09-26 13:08:08 +0800

    test: Add case to testDecimalPointVaild(), Change test function name

--- src/test/java/com/jerryio/protocol_diagram/test/token/DecimalPointTest.java
+++ src/test/java/com/jerryio/protocol_diagram/test/token/DecimalPointTest.java
@@ -9,18 +9,22 @@ import com.jerryio.protocol_diagram.token.DecimalPoint;
 
 public class DecimalPointTest {
     @Test
-    public void testDigit1To9Vaild() {
+    public void testDecimalPointVaild() {
+        assertEquals(DecimalPoint.parse(new CodePointBuffer(".")), new DecimalPoint('.'));
+        assertEquals(DecimalPoint.parse(new CodePointBuffer(". ")), new DecimalPoint('.'));
+        assertEquals(DecimalPoint.parse(new CodePointBuffer(".a")), new DecimalPoint('.'));
         assertEquals(DecimalPoint.parse(new CodePointBuffer(".123")), new DecimalPoint('.'));
     }
 
     @Test
-    public void testDigit1To9Null(){
+    public void testDecimalPointNull(){
+        assertNull(DecimalPoint.parse(new CodePointBuffer(" .")));
         assertNull(DecimalPoint.parse(new CodePointBuffer("0")));
         assertNull(DecimalPoint.parse(new CodePointBuffer(" ")));
         assertNull(DecimalPoint.parse(new CodePointBuffer("a")));
         assertNull(DecimalPoint.parse(new CodePointBuffer("")));
         assertNull(DecimalPoint.parse(new CodePointBuffer("1 ")));
         assertNull(DecimalPoint.parse(new CodePointBuffer("-1")));
-        assertNull(DecimalPoint.parse(new CodePointBuffer("10")));
+        assertNull(DecimalPoint.parse(new CodePointBuffer("1")));
     }
 }
