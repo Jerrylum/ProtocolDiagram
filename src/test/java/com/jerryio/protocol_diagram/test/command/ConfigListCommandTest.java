922b1c46c9ea09fac2d09dad36fa0740598f0d3a
crapper <crapper.612@gmail.com>
2022-10-05 11:00:44 +0800

    test: add ConfigListCommandTest

---
+++ src/test/java/com/jerryio/protocol_diagram/test/command/ConfigListCommandTest.java
@@ -0,0 +1,34 @@
+package com.jerryio.protocol_diagram.test.command;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.jerryio.protocol_diagram.command.ConfigListCommand;
+import com.jerryio.protocol_diagram.token.CodePointBuffer;
+import com.jerryio.protocol_diagram.token.CommandLine;
+import com.jerryio.protocol_diagram.command.HandleResult;
+
+
+public class ConfigListCommandTest {
+    @Test
+    public void testConfigListCommandGet() {
+        ConfigListCommand clc = new ConfigListCommand();
+        assertEquals(clc.getName(), "config");
+        assertEquals(clc.getUsage(), "");
+        assertEquals(clc.getDescription(), "List all settings");
+    }
+
+    @Test
+    public void testConfigListCommandHandleSuccess() {
+        ConfigListCommand lc = new ConfigListCommand();
+        assertEquals(lc.handle(CommandLine.parse(new CodePointBuffer("config"))).success(), true);
+    }
+
+    @Test
+    public void testConfigListCommandHandleFail() {
+        ConfigListCommand lc = new ConfigListCommand();
+        assertEquals(lc.handle(CommandLine.parse(new CodePointBuffer("config test"))), HandleResult.NOT_HANDLED);
+        assertEquals(lc.handle(CommandLine.parse(new CodePointBuffer("test"))), HandleResult.NOT_HANDLED);
+    }
+}
