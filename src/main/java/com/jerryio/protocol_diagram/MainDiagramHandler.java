16911f3b40508c9d3d5752a318d3cb288daf260f
JohnDoeAntler <16289028+JohnDoeAntler@users.noreply.github.com>
2022-10-30 06:16:30 +0800

    docs: add javadoc comment on `MainDiagramHandler`

--- src/main/java/com/jerryio/protocol_diagram/MainDiagramHandler.java
+++ src/main/java/com/jerryio/protocol_diagram/MainDiagramHandler.java
@@ -21,33 +21,63 @@ public final class MainDiagramHandler extends Timeline<CancellableCommand> {
         newDiagram();
     }
 
+    /**
+     * a instance method that gets the diagram from Main
+     */
     @Override
     public Diagram getDiagram() {
         return Main.diagram;
     }
 
+    /**
+     * a instance method that sets the diagram from Main
+     * @param diagram
+     * @return void
+     */
     public void setDiagram(Diagram diagram) {
         Main.diagram = diagram;
     }
 
+    /**
+     * a method that eliminate all previous diagram related logic, and re-create a new diagram
+     * @return void
+     */
     public void newDiagram() {
         setDiagram(new Diagram());
         sourceFilePath = null;
         resetHistory();
     }
 
+    /**
+     * a method that checks whether the program is modified
+     * @return boolean
+     */
     public boolean isModified() {
         return isModified || (sourceCurrentMemento != getLatestMemento());
     }
 
+    /**
+     * a method that sets whether the program is modified
+     * @param isModified
+     * @return void
+     */
     public void setModified(boolean isModified) {
         this.isModified = isModified;
     }
 
+    /**
+     * a getter function that retrieve the source file path
+     * @return String
+     */
     public String getSourceFilePath() {
         return sourceFilePath;
     }
 
+    /**
+     * a method that create a diagram from a JSON file, eliminate all history and renew the source path
+     * @param path
+     * @return HandleResult
+     */
     public HandleResult load(String path) {
         Diagram diagram = FileUtils.load(path);
 
@@ -61,6 +91,10 @@ public final class MainDiagramHandler extends Timeline<CancellableCommand> {
         }
     }
 
+    /**
+     * a method that creates a JSON file from the current diagram, and set the flag `isModified` to false
+     * @return HandleResult
+     */
     public HandleResult save(String path) {
         HandleResult result = FileUtils.save(path, Main.diagram);
         if (result.success()) {
@@ -72,6 +106,10 @@ public final class MainDiagramHandler extends Timeline<CancellableCommand> {
         return result;
     }
 
+    /**
+     * a method that eliminates all history and set the flag `isModified` to false
+     * @return void
+     */
     @Override
     public void resetHistory() {
         super.resetHistory();
