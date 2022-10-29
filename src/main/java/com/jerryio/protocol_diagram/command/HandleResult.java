cca654ac7e678e2dfbad303521799ff420fce180
JohnDoeAntler <16289028+JohnDoeAntler@users.noreply.github.com>
2022-10-30 06:16:50 +0800

    docs: add javadoc comment on `HandleResult`

--- src/main/java/com/jerryio/protocol_diagram/command/HandleResult.java
+++ src/main/java/com/jerryio/protocol_diagram/command/HandleResult.java
@@ -6,10 +6,20 @@ public record HandleResult(boolean success, String message) {
     public static final HandleResult TOO_FEW_ARGUMENTS = new HandleResult(false, "Too few arguments.");
     public static final HandleResult TOO_MANY_ARGUMENTS = new HandleResult(false, "Too many arguments.");
 
+    /**
+     * a utility function that assist to instantiate a new failed HandleResult with dynamic message
+     * @param message
+     * @return HandleResult
+     */
     public static HandleResult fail(String message) {
         return new HandleResult(false, message);
     }
 
+    /**
+     * a utility function that assist to instantiate a new success HandleResult with dynamic message
+     * @param message
+     * @return HandleResult
+     */
     public static HandleResult success(String message) {
         return new HandleResult(true, message);
     }
