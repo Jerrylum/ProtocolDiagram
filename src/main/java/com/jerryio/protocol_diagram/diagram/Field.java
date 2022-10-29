924790806dba267c2ef1459e408ccc8954c715ef
JohnDoeAntler <16289028+JohnDoeAntler@users.noreply.github.com>
2022-10-30 06:17:46 +0800

    docs: add javadoc comment on `Field`

--- src/main/java/com/jerryio/protocol_diagram/diagram/Field.java
+++ src/main/java/com/jerryio/protocol_diagram/diagram/Field.java
@@ -12,22 +12,41 @@ public class Field {
         this.length = length;
     }
 
+    /**
+     * this constructor will transform a pair of value into then field name and the length of the field
+     * @param pair
+     */
     public Field(Pair<String, Integer> pair) {
         this(pair.first(), pair.second());
     }
 
+    /**
+     * a getter method that get the name of this diagram field
+     * @return
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * a getter method that get the length of this diagram field
+     */
     public int getLength() {
         return length;
     }
 
+    /**
+     * a setter method that sets the name of this diagram field
+     * @param name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * a setter method that sets the length of this diagram field
+     * @param length
+     */
     public void setLength(int length) {
         this.length = length;
     }
