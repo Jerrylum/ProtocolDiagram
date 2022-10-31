990ec18d3dc01e70c3acacc9d088343646eac6ff
JohnDoeAntler <16289028+JohnDoeAntler@users.noreply.github.com>
2022-10-31 14:35:44 +0800

    docs: add javadoc comment on `IVisible`

--- src/main/java/com/jerryio/protocol_diagram/diagram/render/IVisible.java
+++ src/main/java/com/jerryio/protocol_diagram/diagram/render/IVisible.java
@@ -1,5 +1,10 @@
 package com.jerryio.protocol_diagram.diagram.render;
 
 public interface IVisible {
+    /**
+     * a getter method that return the visibility of the element
+     * 
+     * @return boolean
+     */
     public boolean isVisible();
 }
