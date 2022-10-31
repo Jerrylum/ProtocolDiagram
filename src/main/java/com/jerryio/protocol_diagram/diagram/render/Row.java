6ff6a067c2b14aea869a6d417140195aecb6bc2f
JohnDoeAntler <16289028+JohnDoeAntler@users.noreply.github.com>
2022-10-31 14:36:04 +0800

    docs: add javadoc comment on `Row`

--- src/main/java/com/jerryio/protocol_diagram/diagram/render/Row.java
+++ src/main/java/com/jerryio/protocol_diagram/diagram/render/Row.java
@@ -10,6 +10,12 @@ public class Row extends SegmentGroup<RowSegment> {
         super(bit);
     }
 
+    /**
+     * a method for appending a field into the segment group (row)
+     * 
+     * @param field
+     * @return
+     */
     public Row addField(Field field) {
         int consume = Math.min(field.getLength(), bit - used);
         field.setLength(field.getLength() - consume);
@@ -21,6 +27,13 @@ public class Row extends SegmentGroup<RowSegment> {
         return this;
     }
 
+    /**
+     * a method for appending row tail also know as the reserved slot into
+     * the segment group, note that if the used space is equal to the number
+     * of bit it has, no row tail would be appended has the row is already full
+     * 
+     * @param isVisible
+     */
     public void addTail(boolean isVisible) {
         if (used != bit)
             segments.add(new RowTail(used, bit - used, isVisible));
