449d2ef4eded0ca27343ed614cc40ff2f4c16004
JohnDoeAntler <16289028+JohnDoeAntler@users.noreply.github.com>
2022-10-31 14:36:22 +0800

    docs: add javadoc comment on `Element`

--- src/main/java/com/jerryio/protocol_diagram/diagram/render/element/Element.java
+++ src/main/java/com/jerryio/protocol_diagram/diagram/render/element/Element.java
@@ -3,5 +3,13 @@ package com.jerryio.protocol_diagram.diagram.render.element;
 import com.jerryio.protocol_diagram.diagram.render.Matrix;
 
 public abstract class Element {
+    /**
+     * a callback method for shaping the element such that matching the context
+     * of the matrix by the given matrix instance and the position of the current element
+     * 
+     * @param m
+     * @param x
+     * @param y
+     */
     public abstract void process(Matrix m, int x, int y);
 }
