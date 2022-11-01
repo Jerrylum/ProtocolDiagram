fc9a122a25b5bd8836d51ead92f7b2011866664b
JohnDoeAntler <16289028+JohnDoeAntler@users.noreply.github.com>
2022-11-02 03:35:00 +0800

    docs: improve and fix the javadoc comment in `DecimalPoint`

--- src/main/java/com/jerryio/protocol_diagram/token/DecimalPoint.java
+++ src/main/java/com/jerryio/protocol_diagram/token/DecimalPoint.java
@@ -2,6 +2,9 @@ package com.jerryio.protocol_diagram.token;
 
 import com.jerryio.protocol_diagram.util.TokenUtils;
 
+/**
+ * this record is a data class that memorizes the character of a decimal point, which theoretically only be '.'
+ */
 public record DecimalPoint(char value) implements Token {
     public static DecimalPoint parse(CodePointBuffer buffer) {
         return TokenUtils.doParseCodepoint(buffer, new char[] { '.' }, DecimalPoint.class);
