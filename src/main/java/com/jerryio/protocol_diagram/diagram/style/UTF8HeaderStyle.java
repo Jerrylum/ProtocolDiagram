da5fdaaf19926d40c4193e0bbf3861687b5a2e8f
JohnDoeAntler <16289028+JohnDoeAntler@users.noreply.github.com>
2022-11-02 03:34:21 +0800

    docs: improve and fix the javadoc comment in `UTF8HeaderStyle`

--- src/main/java/com/jerryio/protocol_diagram/diagram/style/UTF8HeaderStyle.java
+++ src/main/java/com/jerryio/protocol_diagram/diagram/style/UTF8HeaderStyle.java
@@ -6,8 +6,17 @@ import com.jerryio.protocol_diagram.diagram.render.element.Connector;
 import com.jerryio.protocol_diagram.diagram.render.element.Element;
 import com.jerryio.protocol_diagram.diagram.render.element.NextLine;
 
+/**
+ * this class is a extended style of UTF8, it changes the logic of rendering the first row
+ * of connectors, such that the connect has a spike that goes up for indicating the index,
+ * provides more readability
+ */
 public class UTF8HeaderStyle extends UTF8Style {
-
+    /**
+     * this constructor takes a list of elements and assigns it into private instance variable
+     * 
+     * @param elements a list of elements in the diagram
+     */
     public UTF8HeaderStyle(List<Element> elements) {
         super(elements);
         for (Element e : elements) {
