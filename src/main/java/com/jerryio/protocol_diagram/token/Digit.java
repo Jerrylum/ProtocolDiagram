7a827a991c37f80d190ccf393f8c35847c3737a8
JohnDoeAntler <16289028+JohnDoeAntler@users.noreply.github.com>
2022-11-02 03:35:06 +0800

    docs: improve and fix the javadoc comment in `Digit`

--- src/main/java/com/jerryio/protocol_diagram/token/Digit.java
+++ src/main/java/com/jerryio/protocol_diagram/token/Digit.java
@@ -2,6 +2,9 @@ package com.jerryio.protocol_diagram.token;
 
 import com.jerryio.protocol_diagram.util.TokenUtils;
 
+/**
+ * this record is a data class that memorizes the character of a digit, which theoretically only in the range [0-9]
+ */
 public record Digit(char value) implements Token {
     public static Digit parse(CodePointBuffer buffer) {
         return TokenUtils.doParseCodepoint(buffer, new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' },
