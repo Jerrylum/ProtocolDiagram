719b4f8d6d0cf2737bf13bf41de8a77ddcc98cae
JohnDoeAntler <16289028+JohnDoeAntler@users.noreply.github.com>
2022-11-02 03:33:55 +0800

    docs: improve and fix the javadoc comment in `AsciiVerboseStyle`

--- src/main/java/com/jerryio/protocol_diagram/diagram/style/AsciiVerboseStyle.java
+++ src/main/java/com/jerryio/protocol_diagram/diagram/style/AsciiVerboseStyle.java
@@ -6,8 +6,16 @@ import com.jerryio.protocol_diagram.diagram.render.element.DividerSegment;
 import com.jerryio.protocol_diagram.diagram.render.element.Element;
 import com.jerryio.protocol_diagram.diagram.render.element.Segment;
 
+/**
+ * this class is responsible in transforming the diagram into ASCII verbose style, which
+ * the divider will be interleaved with + and - based on the deviation of the index
+ */
 public class AsciiVerboseStyle extends AsciiStyle {
-
+    /**
+     * this constructor takes a list of elements and assigns it into private instance variable
+     * 
+     * @param elements a list of elements in the diagram
+     */
     public AsciiVerboseStyle(List<Element> elements) {
         super(elements);
     }
