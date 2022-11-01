574eb07400d545178b84c8556b8ebefb3b940f28
JohnDoeAntler <16289028+JohnDoeAntler@users.noreply.github.com>
2022-11-02 03:32:30 +0800

    docs: improve and fix the javadoc comment in `Option`

--- src/main/java/com/jerryio/protocol_diagram/config/Option.java
+++ src/main/java/com/jerryio/protocol_diagram/config/Option.java
@@ -3,8 +3,18 @@ package com.jerryio.protocol_diagram.config;
 import com.jerryio.protocol_diagram.command.HandleResult;
 import com.jerryio.protocol_diagram.token.Parameter;
 
+/**
+ * this abstract class provides a basic shape of an option class, that takes a key
+ * in order to instantiate an option class, once it is set, it could not be changed
+ * afterward
+ */
 public abstract class Option {
 
+    /**
+     * the key of this option, it should be treated as a readonly value,
+     * since by logic, the key of a option should not be able to be changed,
+     * otherwise it would cause confusions
+     */
     private String key;
 
     /**
@@ -20,18 +30,39 @@ public abstract class Option {
     /**
      * a getter method that retrieves the readonly key
      * 
-     * @return
+     * @return String
      */
     public String getKey() {
         return key;
     }
 
+    /**
+     * a setter method that sets the value of this option
+     * 
+     * @param value
+     * @return HandleResult
+     */
     public abstract HandleResult setValue(Parameter value);
 
+    /**
+     * a getter method that retrieves the value of this option
+     * 
+     * @return Object
+     */
     public abstract Object getValue();
 
+    /**
+     * a getter method that retrieves the default value of this option
+     * 
+     * @return Object
+     */
     public abstract Object getDefault();
 
+    /**
+     * a method that returns a manual statement for displaying the usage description
+     * 
+     * @return String
+     */
     public abstract String getUsageDescription();
 
 }
