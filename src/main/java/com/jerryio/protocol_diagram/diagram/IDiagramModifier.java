6d24f6dd426fac8047cde7695f92a31a00ec1ca8
JohnDoeAntler <16289028+JohnDoeAntler@users.noreply.github.com>
2022-11-02 03:33:11 +0800

    docs: improve and fix the javadoc comment in `IDiagramModifier`

--- src/main/java/com/jerryio/protocol_diagram/diagram/IDiagramModifier.java
+++ src/main/java/com/jerryio/protocol_diagram/diagram/IDiagramModifier.java
@@ -1,5 +1,8 @@
 package com.jerryio.protocol_diagram.diagram;
 
+/**
+ * this interface is used to distinguish whether the command will manipulate the diagram instance
+ */
 public interface IDiagramModifier {
 
 }
