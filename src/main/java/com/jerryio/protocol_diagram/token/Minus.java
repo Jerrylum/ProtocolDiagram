002733fa248c5e724359bb12007f6a7429ca8e06
JohnDoeAntler <16289028+JohnDoeAntler@users.noreply.github.com>
2022-11-02 03:35:37 +0800

    docs: improve and fix the javadoc comment in `Minus`

--- src/main/java/com/jerryio/protocol_diagram/token/Minus.java
+++ src/main/java/com/jerryio/protocol_diagram/token/Minus.java
@@ -2,6 +2,9 @@ package com.jerryio.protocol_diagram.token;
 
 import com.jerryio.protocol_diagram.util.TokenUtils;
 
+/**
+ * this record is used to represent the negative sign and the minus symbol
+ */
 public record Minus(char value) implements Token {
     public static Minus parse(CodePointBuffer buffer) {
         return TokenUtils.doParseCodepoint(buffer, new char[] { '-' }, Minus.class);
