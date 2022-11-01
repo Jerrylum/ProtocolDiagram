4ee3846c1b3af0c5820addfbfbb2ffd9a21e0a76
JohnDoeAntler <16289028+JohnDoeAntler@users.noreply.github.com>
2022-11-02 03:29:45 +0800

    docs: improve and fix the javadoc comment in `HandleResult`

--- src/main/java/com/jerryio/protocol_diagram/command/HandleResult.java
+++ src/main/java/com/jerryio/protocol_diagram/command/HandleResult.java
@@ -1,5 +1,9 @@
 package com.jerryio.protocol_diagram.command;
 
+/**
+ * this record that holds the information of the command handling result, including
+ * the state of success and the reason of what driven to this result
+ */
 public record HandleResult(boolean success, String message) {
     public static final HandleResult NOT_HANDLED = new HandleResult(false, null);
     public static final HandleResult HANDLED = new HandleResult(true, null);
